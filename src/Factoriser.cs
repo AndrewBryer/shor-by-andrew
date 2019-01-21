4d83863961e2398940c8080ad7612b750f903d26
andrewbryer <andrew.bryer@thoughtworks.com>
2019-01-21 19:00:20 +0000

    [Andrew] Added selection of random number a and handling of case where gcd != 1

--- src/Factoriser.cs
+++ src/Factoriser.cs
@@ -4,7 +4,6 @@ namespace Shor
 {
     public class Factoriser
     {
-        // Pick a < n
         // Find GCD of a and N
         // If not 1 then we are done
         // Do Quantum stuff until result is not 0
@@ -12,6 +11,10 @@ namespace Shor
         // If r is odd pick a new a
         // If a ^ (r / 2) is -1 mod n then pick a new a
         // Find GCD of (a^(r/2) and N) and of (a^(r/2) and N) these are the factors
+        private GreatestCommonDenominator gcd;
+        public Factoriser() {
+            gcd = new GreatestCommonDenominator();
+        }
         internal (int, int) factorise(int numberToFactorise)
         {
             if (numberToFactorise < 2)
@@ -24,6 +27,18 @@ namespace Shor
             }
             else 
             {
+                return factoriseWithShors(numberToFactorise);
+            }
+        }
+
+        private (int, int) factoriseWithShors(int numberToFactorise)
+        {
+            int a = new Random().Next(3, numberToFactorise);
+            int gcdOfAAndN = gcd.findGCD(a, numberToFactorise);
+            if (gcdOfAAndN != 1) {
+                return (gcdOfAAndN, numberToFactorise / gcdOfAAndN);
+            }
+            else {
                 throw new NotImplementedException();
             }
         }
